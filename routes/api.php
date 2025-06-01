c0a34bfce18ae683ec462c9487050b7271a8a30d
jaezar <tech@broadheader.com>
2025-06-01 23:31:38 +0800

    :bug:

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/telegram/webhook', [ReportController::class, 'handleTelegramWebhook']);
+Route::post('/telegram/webhook/send-report', [ReportController::class, 'webhook']);
 Route::get('weekly-sales', [ReportController::class, 'getWeeklySales']);
+Route::post('/telegram/webhook/generate-report', [ReportController::class, 'handleTelegramWebhook']);
